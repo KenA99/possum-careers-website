d587da2c2dc7effcc5e97c8d56ae78540db4f8d5
KenA99 <97471966+KenA99@users.noreply.github.com>
2023-05-29 16:58:07 +0000

    added another template and dynamic updating

--- app.py
+++ app.py
@@ -1,10 +1,42 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+job_dict = [
+  {
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'Bengaluru, India',
+    'salary': 'Rs. 10,000,000'
+  },
+  {
+    'id': 2,
+    'title': 'Data Scienist',
+    'location': 'Dehli, India',
+    'salary': 'Rs. 15,000,000'
+  },
+  {
+    'id': 3,
+    'title': 'Front-end Developer',
+    'location': 'San Francisco, USA',
+    'salary': '$100,000'
+  },
+  {
+    'id': 4,
+    'title': 'Back-end Developer',
+    'location': 'Remote',
+    # 'salary': 'Rs. 19,000,000'
+  }
+]
+
+
 @app.route('/')
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html', jobs=job_dict)
+
+@app.route('/jobs')
+def list_jobs():
+  return jsonify(job_dict)
 
 if __name__ == "__main__":
-  app.run(host = '0.0.0.0', debug= True)
+  app.run(host='0.0.0.0', debug=True)
